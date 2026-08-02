caca5758e3f44a64eea84abf05c6468d428fd8af
grupocadayasas <saitoudf@icloud.com>
2026-08-02 15:48:54 -0500

    Agregar referencias del dashboard y macrozona

---
+++ dom-extensions-20260802k.js
@@ -0,0 +1,12 @@
+'use strict';
+
+Object.assign(dom, {
+    macroFilter: el('filtroMacrozona'),
+    totalZones: el('totalZonas'),
+    totalMacroZones: el('totalMacrozonas'),
+    statsMacroZone: el('estadisticasMacrozona'),
+    statsCommune: el('estadisticasComuna'),
+    mapDashVisible: el('mapDashVisible'),
+    mapDashZones: el('mapDashZones'),
+    mapDashMacro: el('mapDashMacro')
+});
